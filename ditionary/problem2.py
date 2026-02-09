ad44104176ea12f9fd2b7f3c9b921574d9c7bf92
Rushikesh Sunil Samudre <rishikeshsamudre1@gmail.com>
2026-02-09 19:00:27 +0530

    Create problem2.py
    uploading problem2

---
+++ ditionary/problem2.py
@@ -0,0 +1,10 @@
+info = {
+    "name":"Rushikesh",
+    "subject":["python","java","c","C++"],
+    "topics":("dict","set"),
+    "age":21,
+    "is_adult":True,
+    "mark":96.5
+    
+}
+print(info)
