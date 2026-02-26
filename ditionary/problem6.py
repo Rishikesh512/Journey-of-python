d15eebf4ea41a9a7131c6fd82dbfaa766331b8d8
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-02-26 23:08:09 +0530

    new problem

---
+++ ditionary/problem6.py
@@ -0,0 +1,9 @@
+person = {}
+
+person["name"] = input("Enter name:-")
+person["age"] = input("Enter age:-")
+person["city"]= input("Enter city:-")
+person["salary"] = input("Enter")
+
+
+print("Person Detail:",person)
