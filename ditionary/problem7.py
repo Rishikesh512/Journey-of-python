7f500b12478bf3327eb615d330f4371150d661a3
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-02-26 23:34:44 +0530

    student details

---
+++ ditionary/problem7.py
@@ -0,0 +1,13 @@
+student = {}
+
+name = input("Enter student name :-")
+age = input("Enter studet age :-")
+standerd =input("Enter standerd of student :-")
+course = input("Enter course of student :-")
+
+student["name"] = name
+student["age"] = age
+student["standerd"] = standerd
+student["course"] = course
+
+print("Stdent details :-",student)
