55c4a14282cc7aba6a2560b99f06922116b21613
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-03-01 15:30:48 +0530

    Pen and pencil class

---
+++ OOP/polymorphism/problem4.py
@@ -0,0 +1,13 @@
+class Pen:
+    def use(self):
+        print("Writing with pen")
+
+class Pencil:
+    def use(self):
+        print("Writing with pencil")
+
+pen = Pen()
+pencil =Pencil()
+
+pen.use()
+pencil.use()
