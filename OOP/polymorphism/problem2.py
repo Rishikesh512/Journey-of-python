4376e43450eb1fd22f9c13039776d0d04f3f1adb
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-03-01 15:12:24 +0530

    another one

---
+++ OOP/polymorphism/problem2.py
@@ -0,0 +1,20 @@
+class Car:
+    def move(self):
+        print("Car is moving")
+
+class Bike:
+    def move(self):
+        print("Bike is moving")
+
+class Airoplane:
+    def move(self):
+        print("Airoplane is Flying")
+
+c = Car()
+b = Bike()
+a = Airoplane()
+
+
+c.move()
+b.move()
+a.move()
