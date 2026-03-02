4d2156f1d17867f4e9f40c6a2bb65b4c3b80c01f
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-03-02 21:21:51 +0530

    counting vovels in string

---
+++ control_flow/problem10.py
@@ -0,0 +1,28 @@
+
+
+
+        #Count the number of vowels in a given string
+name = input("Enter the name :-").lower()
+char = ("a,e,i,o,u")
+count = 0
+for i in range (0,len(name)):
+
+    if name[i] == "a":
+        count += 1
+    elif name[i] =="e":
+        count +=1
+    elif name[i] =="i":
+        count +=1
+    elif name[i] == "o":
+        count +=1
+    elif name[i] =="u":
+        count +=1
+
+print("The number of vowels in given string is :-",count)
+
+    
+
+
+
+
+
