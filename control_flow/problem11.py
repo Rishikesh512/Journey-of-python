d88f0a9f806f99896963d83159c3ffeea0de9098
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-03-04 10:02:58 +0530

    number divisible by both 5 & 11

---
+++ control_flow/problem11.py
@@ -0,0 +1,8 @@
+number = int(input("Enter any number :-"))
+
+if number % 5 == 0 and number % 11 == 0 :
+
+    print("number is divisible by both 5 and 11")
+
+else:
+    print("numberr is not divisible by both 5 and 11")
