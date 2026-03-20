de95516e9d2e5fa6616d4bf965438472cc1a3252
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-03-20 23:38:24 +0530

    Basic Shape Area

---
+++ OOP/problem24.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+import math
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return math.pi * self.radius * self.radius
+
+c = Circle(5)
+print("Area of Circle:", c.area())
