e61f059e7f25dfabf890fb9ce6061f04653eff74
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-03-21 23:22:16 +0530

    person age

---
+++ OOP/problem31.py
@@ -0,0 +1,19 @@
+class Person:
+    def __init__(self):
+        self.__age = 0
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value > 0:
+            self.__age = value
+        else:
+            print("Invalid age")
+
+
+p = Person()
+p.age = 25
+print("Age:", p.age)
