33be33f71156db5de5e44b47c96c28dd48401d43
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-03-25 22:01:59 +0530

    Longest Consecutive Sequence

---
+++ set/problem23.py
@@ -0,0 +1,17 @@
+nums = [100, 4, 200, 1, 3, 2]
+
+num_set = set(nums)
+longest = 0
+
+for num in num_set:
+    if num - 1 not in num_set:
+        current = num
+        length = 1
+
+        while current + 1 in num_set:
+            current += 1
+            length += 1
+
+        longest = max(longest, length)
+
+print("Longest consecutive length:", longest)
