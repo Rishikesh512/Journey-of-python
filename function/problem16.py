82e7d476d6fda72b4e7299c465a8c9cc3a9d6d35
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-03-29 21:22:41 +0530

    Function to Find All Pairs with Given Sum

--- function/problem16.py
+++ function/problem16.py
@@ -2,3 +2,16 @@ def count_substring(s, sub):
     return s.count(sub)
 
 print(count_substring("banana", "an"))
+def find_pairs(lst, target):
+    pairs = []
+    seen = set()
+    
+    for num in lst:
+        diff = target - num
+        if diff in seen:
+            pairs.append((diff, num))
+        seen.add(num)
+        
+    return pairs
+
+print(find_pairs([1, 2, 3, 4, 5], 6))
