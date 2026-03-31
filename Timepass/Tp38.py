80b9d09abbbe5db32d49dd29c662e5c8c8f3c93e
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-03-31 12:50:07 +0530

    nishu

--- Timepass/Tp38.py
+++ Timepass/Tp38.py
@@ -11,23 +11,24 @@ class Vehicle :
 class electric_car(Vehicle):
 
     def __init__(self,brand,speed):
+        super().__init__(brand,speed)
         print("This car runs on electric Engine")
-        super().__init__(self)
     
 
 class petrol_car(Vehicle):
     def __init__(self,brand,speed):
-        super().__init__(self,brand,speed)
+        super().__init__(brand,speed)
         print("this car run on petrol engine ")
 
 class bicycle(Vehicle):
+    def __init__(self,brand,speed):
+        super().__init__(brand,speed)
+        print("pedal")
 
-    print("pedal")
 
-
-e_car= ("tesla",20)
-p_car = ("Tata",50)
-b = ("Hero",10)
+e_car= electric_car("tesla",20)
+p_car = petrol_car("Tata",50)
+b = bicycle("Hero",10)
 
 e_car.fuel_type()
 p_car.fuel_type()
