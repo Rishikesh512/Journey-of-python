a9dcf91071ca1019623f6c88b4a4499eec091316
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-04-04 22:07:56 +0530

    Elements present in all lists

--- set/problem29.py
+++ set/problem29.py
@@ -1,12 +1,1 @@
-lists = [[1,2,3], [2,3,4], [3,4,5]]
-k = 2
 
-all_elements = set(sum(lists, []))
-result = set()
-
-for x in all_elements:
-    count = sum(1 for lst in lists if x in lst)
-    if count == k:
-        result.add(x)
-
-print("Exactly in k lists:", result)
