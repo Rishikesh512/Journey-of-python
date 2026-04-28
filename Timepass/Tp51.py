db62355566e6f954d740332778df40ff53bc3eb1
Rushikesh512 <rishikeshsamudre1@gmail.com>
2026-04-28 22:40:27 +0530

    marks

---
+++ Timepass/Tp51.py
@@ -0,0 +1,14 @@
+#Take input mark andprint grade
+mark = int(input("enterr the mark:"))
+
+if mark >= 80 :
+    print("Grade:A")
+
+elif mark >=60 :
+    print("Grade:B")
+
+elif mark >=40 :
+    print("Grade:c")
+
+else:
+    print("Fail")
